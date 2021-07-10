fdf8a743713ef4952e8216bd1a44e7b533c871e7
David Carmo <davidsc2812@gmail.com>
2021-07-09 21:04:10 -0300

    Resolucao do Desafio 02

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,63 @@
-export function Content() {
-  // Complete aqui
+import "../styles/content.scss";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+interface ContentProps {
+  selectedGenreId: number;
+}
+interface GenreResponse {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+export function Content({ selectedGenreId }: ContentProps) {
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponse>(
+    {} as GenreResponse
+  );
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+
+    api.get<GenreResponse>(`genres/${selectedGenreId}`).then((response) => {
+      setSelectedGenre(response.data);
+    });
+  }, [selectedGenreId]);
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
